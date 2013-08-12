7e090129c2cfa162d392f676571dd0dd5eaf7590
megakuzmitch <support@amobile-studio.ru>
2013-08-12 09:55:40 +0600

    fix admin_menu

--- protected/modules/admin/views/layouts/main.php
+++ protected/modules/admin/views/layouts/main.php
@@ -20,53 +20,55 @@
 			 */
 			$menuItems = array(
 				array('label'=>'Настройки', 'url'=>'/admin'),
-				array('label'=>'Сотрудники', 'url'=>'#', 'items' => array(
-					array('label'=>'Создать', 'url'=>"/admin/employees/create"),
-					array('label'=>'Список', 'url'=>"/admin/employees/list"),
-				)),
-				array('label'=>'Новости', 'url'=>'#', 'items' => array(
-					array('label'=>'Создать', 'url'=>"/admin/events/create"),
-					array('label'=>'Список', 'url'=>"/admin/events/list"),
-				)),
-				array('label'=>'Рестораны', 'url'=>'#', 'items' => array(
-					array('label'=>'Создать', 'url'=>"/admin/places/create"),
-					array('label'=>'Список', 'url'=>"/admin/places/list"),
-				)),
-				array('label'=>'Слайдер', 'url'=>'#', 'items' => array(
-					array('label'=>'Создать', 'url'=>"/admin/slider/create"),
-					array('label'=>'Список', 'url'=>"/admin/slider/list"),
-				)),
-				array('label'=>'Страницы', 'url'=>'#', 'items' => array(
-					array('label'=>'Создать', 'url'=>"/admin/pages/create"),
-					array('label'=>'Список', 'url'=>"/admin/pages/list"),
-				)),
-				array('label'=>'Реклама', 'url'=>'#', 'items' => array(
-					array('label'=>'Создать', 'url'=>"/admin/banners/create"),
-					array('label'=>'Список', 'url'=>"/admin/banners/list"),
-				)),
-				array('label'=>'Отзывы', 'url'=>'#', 'items' => array(
-					array('label'=>'Добавить', 'url'=>"/admin/reviews/create"),
-					array('label'=>'Список', 'url'=>"/admin/reviews/list"),
-				)),
-				array('label'=>'Меню', 'url'=>'#', 'items' => array(
-					array('label'=>'Добавить', 'url'=>"/admin/menu/create"),
-					array('label'=>'Список', 'url'=>"/admin/menu/list"),
-				)),
-				array('label'=>'Блюда', 'url'=>'#', 'items' => array(
-					array('label'=>'Добавить', 'url'=>"/admin/dishes/create"),
-					array('label'=>'Список', 'url'=>"/admin/dishes/list"),
-				)),
-				array('label'=>'Вакансии', 'url'=>'#', 'items' => array(
-					array('label'=>'Добавить', 'url'=>"/admin/jobs/create"),
-					array('label'=>'Список', 'url'=>"/admin/jobs/list"),
-				)),
-				array('label'=>'Партнеры', 'url'=>'#', 'items' => array(
-					array('label'=>'Добавить', 'url'=>"/admin/partners/create"),
-					array('label'=>'Список', 'url'=>"/admin/partners/list"),
-				)),
-				array('label'=>'Интерьер', 'url'=>'#', 'items' => array(
-					array('label'=>'Добавить', 'url'=>"/admin/interiors/create"),
-					array('label'=>'Список', 'url'=>"/admin/interiors/list"),
+				array('label'=>'Таблицы', 'url'=>'#', 'items' => array(
+					array('label'=>'Сотрудники', 'url'=>'#', 'items' => array(
+						array('label'=>'Создать', 'url'=>"/admin/employees/create"),
+						array('label'=>'Список', 'url'=>"/admin/employees/list"),
+					)),
+					array('label'=>'Новости', 'url'=>'#', 'items' => array(
+						array('label'=>'Создать', 'url'=>"/admin/events/create"),
+						array('label'=>'Список', 'url'=>"/admin/events/list"),
+					)),
+					array('label'=>'Рестораны', 'url'=>'#', 'items' => array(
+						array('label'=>'Создать', 'url'=>"/admin/places/create"),
+						array('label'=>'Список', 'url'=>"/admin/places/list"),
+					)),
+					array('label'=>'Слайдер', 'url'=>'#', 'items' => array(
+						array('label'=>'Создать', 'url'=>"/admin/slider/create"),
+						array('label'=>'Список', 'url'=>"/admin/slider/list"),
+					)),
+					array('label'=>'Страницы', 'url'=>'#', 'items' => array(
+						array('label'=>'Создать', 'url'=>"/admin/pages/create"),
+						array('label'=>'Список', 'url'=>"/admin/pages/list"),
+					)),
+					array('label'=>'Реклама', 'url'=>'#', 'items' => array(
+						array('label'=>'Создать', 'url'=>"/admin/banners/create"),
+						array('label'=>'Список', 'url'=>"/admin/banners/list"),
+					)),
+					array('label'=>'Отзывы', 'url'=>'#', 'items' => array(
+						array('label'=>'Добавить', 'url'=>"/admin/reviews/create"),
+						array('label'=>'Список', 'url'=>"/admin/reviews/list"),
+					)),
+					array('label'=>'Меню', 'url'=>'#', 'items' => array(
+						array('label'=>'Добавить', 'url'=>"/admin/menu/create"),
+						array('label'=>'Список', 'url'=>"/admin/menu/list"),
+					)),
+					array('label'=>'Блюда', 'url'=>'#', 'items' => array(
+						array('label'=>'Добавить', 'url'=>"/admin/dishes/create"),
+						array('label'=>'Список', 'url'=>"/admin/dishes/list"),
+					)),
+					array('label'=>'Вакансии', 'url'=>'#', 'items' => array(
+						array('label'=>'Добавить', 'url'=>"/admin/jobs/create"),
+						array('label'=>'Список', 'url'=>"/admin/jobs/list"),
+					)),
+					array('label'=>'Партнеры', 'url'=>'#', 'items' => array(
+						array('label'=>'Добавить', 'url'=>"/admin/partners/create"),
+						array('label'=>'Список', 'url'=>"/admin/partners/list"),
+					)),
+					array('label'=>'Интерьер', 'url'=>'#', 'items' => array(
+						array('label'=>'Добавить', 'url'=>"/admin/interiors/create"),
+						array('label'=>'Список', 'url'=>"/admin/interiors/list"),
+					)),
 				)),
 			)
 		?>
