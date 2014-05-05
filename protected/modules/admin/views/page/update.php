d8b9dc86ab955ff077c90c3e66977f0b371133e4
megakuzmitch <maxro.kuzmitch@gmail.com>
2014-05-05 09:28:48 +0600

    add back url to page

--- protected/modules/admin/views/page/update.php
+++ protected/modules/admin/views/page/update.php
@@ -7,7 +7,6 @@ $this->breadcrumbs=array(
 $this->menu=array(
     array('label'=>'Структура сайта','url'=>array('/admin/structure/list', 'opened' => $model->node->id)),
     array('label'=>'Добавить новую','url'=>array('create', 'node_id'=>$model->node->id)),
-	array('label'=>'← Раздел', 'url'=>array('/admin/structure/update', 'id'=>$model->node->id)),
 );
 if ( $model->node ) {
     $this->menu[] = array('label'=>'← К разделу', 'url'=>array('/admin/structure/update', 'id' => $model->node->id));
